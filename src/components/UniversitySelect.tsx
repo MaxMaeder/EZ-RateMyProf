53e63a26161de7f03eb873162cf2c60180f1e9dc
Max Maeder <max.s.maeder@gmail.com>
2023-07-19 10:36:35 -0500

    Revert UniversitySelect

--- src/components/UniversitySelect.tsx
+++ src/components/UniversitySelect.tsx
@@ -1,20 +1,21 @@
 import { Select } from "@mantine/core";
 import { useMemo } from "react";
 
-import { useStorage } from "@plasmohq/storage/hook";
-
 import { universities } from "~assets/universities";
 
-const UniversitySelect = () => {
-  const [university, setUniversity] = useStorage("university");
+type UniversitySelectProps = {
+  value: string | undefined;
+  onChange: (v: string) => void;
+};
 
+const UniversitySelect = ({ value, onChange }: UniversitySelectProps) => {
   const allUniversities = useMemo(() => {
     let _allUniv = [...new Set(universities)];
 
-    if (university && !_allUniv.includes(university)) _allUniv.push(university);
+    if (value && !_allUniv.includes(value)) _allUniv.push(value);
 
     return _allUniv;
-  }, [universities, university]);
+  }, [universities, value]);
 
   return (
     <Select
@@ -26,11 +27,11 @@ const UniversitySelect = () => {
       getCreateLabel={(query) => `Other: '${query}'`}
       required
       data={allUniversities}
-      value={university}
+      value={value}
       maxDropdownHeight={150}
-      onChange={(u) => setUniversity(u)}
+      onChange={(u) => onChange(u)}
       onCreate={(u) => {
-        setUniversity(u);
+        onChange(u);
         return u;
       }}
     />
